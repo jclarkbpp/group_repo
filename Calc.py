265d0dda278c3491ccdf4bc039767bc8c4024a9b
rose-84 <r.hincks@my.bpp.com>
2025-07-18 12:35:39 +0100

    splitfiles

--- Calc.py
+++ Calc.py
@@ -10,33 +10,3 @@ def multiply(num_1, num_2):
 
 def subtract(num_1, num_2):
     return num_1 - num_2
-
-
-
-
-
-repeat = True
-while repeat == True:  
-    user_input1 = int(input("Input Nunber 1: "))
-    user_input2 = int(input("Input Number 2: "))
-    operation1 = input("Input operation (+-*/): ")
-    try: 
-        if operation1 == "+":
-            print(f"{user_input1} + {user_input2} = {add(user_input1, user_input2)}")
-        elif operation1 == "-":
-            print(f"{user_input1} - {user_input2} = {subtract(user_input1, user_input2)}")
-        elif operation1 == "*":
-            print(f"{user_input1} * {user_input2} = {multiply(user_input1, user_input2)}")
-        elif operation1 == "/":
-            print(f"{user_input1} / {user_input2} = {divide(user_input1, user_input2)}")
-        
-        user = input("Want do another caluculation? y/n")
-        if user == "n":
-            repeat = False
-    except:
-        user = input("Invalid operation: Want to try again? y/n")
-        if user == "n":
-            repeat = False
-            break
-
-    
--- test_calc.py
+++ test_calc.py
@@ -1,5 +1,5 @@
 import pytest
-from calc import add, divide, multiply, subtract
+from Calc import add, divide, multiply, subtract
 
 def test_add():
     result = add(10, 5)

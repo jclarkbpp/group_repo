f49f089e178aa432cf0a8d51291e8e1380ac9e1a
rose-84 <r.hincks@my.bpp.com>
2025-07-18 12:17:16 +0100

    testfixed

--- test_calc.py
+++ test_calc.py
@@ -1,19 +1,19 @@
 import pytest
 from calc import add, divide, multiply, subtract
 
-def test_add(self):
+def test_add():
     result = add(10, 5)
     assert result == 15
 
-def test_subtract(self):
+def test_subtract():
     result = subtract(10, 5)
     assert result == 5
     
-def test_multiply(self):
+def test_multiply():
     result = multiply(10, 5)
     assert result == 50
 
-def test_divide(self):
+def test_divide():
     result = divide(10, 5)
     assert result == 2
 
